c1dc5179278a58fdb9f7a4b0808f7b07b9bbf05a
Robert Wiley <rbtylee@gmail.com>
2023-06-18 08:34:03 -0400

    Keep pkg versions up to date

--- Ubuntu/jammy.install.sh
+++ Ubuntu/jammy.install.sh
@@ -109,7 +109,7 @@ cd "$TEMP_DIR" || exit 1
 wget http://packages.bodhilinux.com/bodhi/pool/b7main/b/bodhilinux-keyring/bodhilinux-keyring_2022.11.07_all.deb
 wget http://packages.bodhilinux.com/bodhi/pool/b7main/u/ubuntu-system-adjustments/ubuntu-system-adjustments_0.0.1-1_all.deb
 wget http://packages.bodhilinux.com/bodhi/pool/b7main/b/bodhi-info-moksha/bodhi-info-moksha_0.0.1-1_all.deb
-wget http://packages.bodhilinux.com/bodhi/pool/b7main/b/bodhi-settings/bodhi-settings_0.0.1-5_all.deb
+wget http://packages.bodhilinux.com/bodhi/pool/b7main/b/bodhi-settings/bodhi-settings_0.0.1-6_all.deb
 
 sudo apt -y --no-install-recommends install ./*.deb
 
@@ -140,5 +140,8 @@ else
   echo "Installing Bodhi's apturl"
   sudo apt -y install apturl-saf
 fi
+
+# Uncomment if you need it
+# sudo apt -y install nvidia-ppa
 
 exit 0
